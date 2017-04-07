cb3af14ef6b7b8dc11f3f4cb720d8242ce8c1b2e
wython <1098552657@qq.com>
2017-04-07 20:32:41 +0800

    aaa

--- frond-end/src/js/pages/HomePage/index.js
+++ frond-end/src/js/pages/HomePage/index.js
@@ -3,11 +3,18 @@
  */
 
 import React from 'react'
+import './homepage.css'
 
 export default class HomePage extends React.Component {
     render() {
         return (
-            <div>i am a home page</div>
+            <div className="home-page">
+                <div className="banner-wrapper">
+
+                </div>
+                <div></div>
+                <div></div>
+            </div>
         )
     }
 }
